5dcd24f45a72402d7f7e3461f70983ee1836164d
gargriya01 <72300799+gargriya01@users.noreply.github.com>
2024-12-26 15:39:28 +0530

    captain logout, profile done

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,9 +1,9 @@
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
-// const blackListTokenModel = require("../models/blacklistToken.model");
+const blackListTokenModel = require("../models/blacklistToken.model");
 const { validationResult } = require("express-validator");
 
-module.exports.registerCaptain = async (req, res, next) => {
+module.exports.registerCaptain = async (req, res, next) => { 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
